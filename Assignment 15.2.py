dcc375ff9219b82181fd9d6e193854b917289960
ldnup <44421144+ldn005402@users.noreply.github.com>
2021-04-18 00:15:28 -0700

    Update Assignment 15.2.py

--- Assignment 15.2.py
+++ Assignment 15.2.py
@@ -1,1 +1,16 @@
-print("15.2")
+CREATE TABLE Ages ( 
+  name VARCHAR(128), 
+  age INTEGER
+)
+
+DELETE FROM Ages;
+INSERT INTO Ages (name, age) VALUES ('Saarah', 40);
+INSERT INTO Ages (name, age) VALUES ('Nuala', 14);
+INSERT INTO Ages (name, age) VALUES ('Liall', 35);
+INSERT INTO Ages (name, age) VALUES ('Elvita', 30);
+INSERT INTO Ages (name, age) VALUES ('Norman', 27);
+INSERT INTO Ages (name, age) VALUES ('Nadine', 31);
+
+SELECT hex(name || age) AS X 
+FROM Ages 
+ORDER BY X
